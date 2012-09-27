fa23e3e95906f4415650c8a8b8797fda7ff8fb10
Jon Lønne <jon@funkbit.no>
2012-09-27 15:01:45 +0200

    Include all packages when distributing.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 from groove import __version__
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Publish to Pypi
 if sys.argv[-1] == 'publish':
@@ -18,7 +18,7 @@ setup(name='django-groove',
     author_email='post@funkbit.no',
     url='https://github.com/funkbit/django-groove',
     include_package_data=True,
-    packages=['groove', 'groove.utils'],
+    packages=find_packages(),
     tests_require=['django>=1.4,<1.5'],
     license='BSD',
     classifiers = (
